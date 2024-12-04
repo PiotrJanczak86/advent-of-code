e2fd94d5effffe8169f2a2b397e280ca074b7648
Piotr Janczak <piotr.janczak1112@gmail.com>
2024-12-04 20:27:44 +0100

    1.1

--- src/main/java/com/advent/AdventApplication.java
+++ src/main/java/com/advent/AdventApplication.java
@@ -1,5 +1,6 @@
 package com.advent;
 
+import com.advent.dayOne.ListSort;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,7 @@ public class AdventApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdventApplication.class, args);
 
+        ListSort listSort = new ListSort();
+        listSort.getNumber();
     }
 }
---
+++ src/main/java/com/advent/dayOne/ListSort.java
@@ -0,0 +1,55 @@
+package com.advent.dayOne;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.*;
+
+public class ListSort {
+
+    public void getNumber() {
+
+        int result = 0;
+        ClassLoader classLoader = ListSort.class.getClassLoader();
+        URL resource = classLoader.getResource("1.1.txt");
+        boolean isItFirst = true;
+        List<Integer> firstList = new ArrayList<>();
+        List<Integer> secondList = new ArrayList<>();
+
+        if (resource != null) {
+            try (Scanner scanner = new Scanner(new File(resource.getFile()))) {
+
+                while (scanner.hasNextInt()) {
+                    int number = scanner.nextInt();
+
+                    if (isItFirst) {
+                        firstList.add(number);
+                        isItFirst = false;
+                    } else {
+                        secondList.add(number);
+                        isItFirst = true;
+                    }
+                }
+
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found: " + e.getMessage());
+            }
+        } else {
+            System.out.println("File not found in resources!");
+        }
+
+        Collections.sort(firstList);
+        Collections.sort(secondList);
+
+        for (int i = 0; i < firstList.size(); i++) {
+            result = result + Math.abs(firstList.get(i) - secondList.get(i));
+        }
+        System.out.println(firstList.size());
+        System.out.println(secondList.size());
+        System.out.println(firstList);
+        System.out.println(secondList);
+
+        System.out.println(result);
+    }
+
+}
